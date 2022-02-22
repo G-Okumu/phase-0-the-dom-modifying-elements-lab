2a6e7a580187ec41451824d5596a7d69b475db6f
George-Okumu <gokumu12@gmail.com>
2022-02-22 19:32:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let removedMain = document.getElementById("main");
+removedMain.remove();
+
+let newHeader = document.createElement("h1");
+let newHeaderOfIdVictoryWithText = newHeader.setAttribute("id", "victory");
+let text = newHeader.innerHTML = "George is the champion";
+document.getElementById("victory").appendChild(text);
